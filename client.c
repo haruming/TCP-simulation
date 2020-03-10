568ea6320658c601a6a2cd6b34f1af54d922110a
haruming <474285697@qq.com>
2020-03-10 01:00:58 -0500

    comment added

--- client.c
+++ client.c
@@ -5,14 +5,22 @@
 int main(int argc, char* argv[]) {
     int fd;
     if (argc > 2) {
+        /* send SYN */
+        /* recvfrom SYN/ACK */
+        /* send ACK */
         fd = cli_connect(argv[1], (short)atoi(argv[2]));
         /* connection fail */
         if (fd < 0) {
             perror("client fail to connect to the server");
         }
         printf("Complete the 3 way handshake\n");
+
+        /* recvfrom FIN -> Close Wait(passive close) */
         receive_packet(fd);
+        /* immediately send ACK */
         if (isfin(fd)) {
+            /* finish final processing, then send FIN -> LAST_ACK */
+            /* recvfrom ACK -> CLOSED */
             close(fd);
         }
         printf("Complete the four way hand waves\n");
--- server.c
+++ server.c
@@ -4,15 +4,23 @@
 
 int main(int argc, char* argv[]) {
     int fd;
+    /* recvfrom SYN */
+    /* send SYN/ACK */
+    /* recvfrom ACK */
     fd = srv_listen(SERVER_PORT);
     if (fd < 0) {
         perror("server fail to listen to the port");
     }
-
+    printf("Server complete the 3 way handshakes\n");
+    
+    /* send FIN -> FIN_WAIT_1 */
+    /* recvfrom ACK -> FIN_WAIT_2 */
     close(fd);
+    /* recvfrom FIN -> TIME_WAIT */
     receive_packet(fd);
+    /* send final ACK -> CLOSED */
     if (isfin(fd)) {
-        printf("Complete the four way hand waves\n");
+        printf("Server complete the four way hand waves\n");
     }
     return 0;
 }
